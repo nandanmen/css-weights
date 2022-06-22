468994688f6a83011b7dd43e876e41967517c903
Nanda <nanda.s@hey.com>
2022-06-22 14:13:18 -0700

    fix: use weird vercel dir

--- app/utils.server.ts
+++ app/utils.server.ts
@@ -1,21 +1,17 @@
 import fs from "fs";
-import path from "path";
 import { marked } from "marked";
 const shiki = require("shiki");
 
 export const getHighlighter = () => {
   return shiki.getHighlighter({
     theme: JSON.parse(
-      fs.readFileSync(
-        path.join(process.cwd(), "app/codesandbox-dark.json"),
-        "utf-8"
-      )
+      fs.readFileSync(`${__dirname}/../../app/codesandbox-dark.json`, "utf-8")
     ),
   });
 };
 
 export const parseDocs = () => {
   return marked.parse(
-    fs.readFileSync(path.join(process.cwd(), "app/help.md"), "utf-8")
+    fs.readFileSync(`${__dirname}/../../app/help.md`, "utf-8")
   );
 };
