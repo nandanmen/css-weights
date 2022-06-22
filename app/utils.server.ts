e6d94e3a29d3bfe3ad57c28a010748b43039874d
Nanda <nanda.s@hey.com>
2022-06-22 14:16:44 -0700

    fix: use path.join

--- app/utils.server.ts
+++ app/utils.server.ts
@@ -1,17 +1,21 @@
 import fs from "fs";
+import path from "path";
 import { marked } from "marked";
 const shiki = require("shiki");
 
 export const getHighlighter = () => {
   return shiki.getHighlighter({
     theme: JSON.parse(
-      fs.readFileSync(`${__dirname}/../../app/codesandbox-dark.json`, "utf-8")
+      fs.readFileSync(
+        path.join(__dirname, "../../", "app/codesandbox-dark.json"),
+        "utf-8"
+      )
     ),
   });
 };
 
 export const parseDocs = () => {
   return marked.parse(
-    fs.readFileSync(`${__dirname}/../../app/help.md`, "utf-8")
+    fs.readFileSync(path.join(__dirname, "../../", "app/help.md"), "utf-8")
   );
 };
