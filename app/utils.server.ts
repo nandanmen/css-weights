13699e7189f77d488143322107fc34cdf54d289c
Nanda <nanda.s@hey.com>
2022-06-22 14:46:23 -0700

    debug by printing dir

--- app/utils.server.ts
+++ app/utils.server.ts
@@ -1,6 +1,18 @@
+import fs from "fs";
 import { fetch } from "@remix-run/node";
 import { marked } from "marked";
 const shiki = require("shiki");
+
+const readDir = async (dir: string) => {
+  const files = await fs.promises.readdir(dir, { withFileTypes: true });
+  files.map(async (file) => {
+    if (file.isDirectory()) {
+      readDir(`${dir}/${file.name}`);
+    } else {
+      console.log(`${dir}/${file.name}`);
+    }
+  });
+};
 
 export const getHighlighter = async () => {
   const resp = await fetch(
@@ -11,6 +23,7 @@ export const getHighlighter = async () => {
 };
 
 export const parseDocs = async () => {
+  readDir(__dirname);
   const resp = await fetch("https://css-weights.vercel.app/help.md");
   const text = await resp.text();
   return marked.parse(text);
