24c1afbae4eb6299e0cf5a48eca363796dfb472a
Nanda <nanda.s@hey.com>
2022-06-21 22:20:02 -0700

    oh god please work

--- app/utils.server.ts
+++ app/utils.server.ts
@@ -1,15 +1,21 @@
 import fs from "fs";
+import path from "path";
 import { marked } from "marked";
 const shiki = require("shiki");
 
 export const getHighlighter = () => {
   return shiki.getHighlighter({
     theme: JSON.parse(
-      fs.readFileSync(`${process.cwd()}/codesandbox-dark.json`, "utf-8")
+      fs.readFileSync(
+        path.join(process.cwd(), "app/codesandbox-dark.json"),
+        "utf-8"
+      )
     ),
   });
 };
 
 export const parseDocs = () => {
-  return marked.parse(fs.readFileSync(`${process.cwd()}/help.md`, "utf-8"));
+  return marked.parse(
+    fs.readFileSync(path.join(process.cwd(), "app/help.md"), "utf-8")
+  );
 };
