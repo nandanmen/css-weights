6ab55f1bf33004fde48263d129cd2943666d6fa4
Nanda <nanda.s@hey.com>
2022-06-22 14:35:41 -0700

    fix: use fetch

--- app/utils.server.ts
+++ app/utils.server.ts
@@ -1,14 +1,17 @@
-import fs from "fs";
-import path from "path";
+import { fetch } from "@remix-run/node";
 import { marked } from "marked";
 const shiki = require("shiki");
 
-export const getHighlighter = () => {
-  return shiki.getHighlighter({
-    theme: JSON.parse(fs.readFileSync("/codesandbox-dark.json", "utf-8")),
-  });
+export const getHighlighter = async () => {
+  const resp = await fetch(
+    "https://css-weights.vercel.app/codesandbox-dark.json"
+  );
+  const theme = await resp.json();
+  return shiki.getHighlighter({ theme });
 };
 
-export const parseDocs = () => {
-  return marked.parse(fs.readFileSync("/help.md", "utf-8"));
+export const parseDocs = async () => {
+  const resp = await fetch("https://css-weights.vercel.app/help.md");
+  const text = await resp.text();
+  return marked.parse(text);
 };
