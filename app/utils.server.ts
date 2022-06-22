5605081353b1549226c930193bf6838f45375601
Nanda <nanda.s@hey.com>
2022-06-22 15:34:31 -0700

    fix: save theme in memory

--- app/utils.server.ts
+++ app/utils.server.ts
@@ -1,42 +1,19 @@
-import fs from "fs";
 import { fetch } from "@remix-run/node";
 import { marked } from "marked";
+
+import { theme } from "./theme.server";
+import { css } from "./lang.server";
+
 const shiki = require("shiki");
 
-const cache = {} as Record<string, unknown>;
-
-const readDir = async (dir: string) => {
-  const files = await fs.promises.readdir(dir, { withFileTypes: true });
-  files.map(async (file) => {
-    if (file.isDirectory()) {
-      readDir(`${dir}/${file.name}`);
-    } else {
-      console.log(`${dir}/${file.name}`);
-    }
-  });
-};
-
 export const getHighlighter = async () => {
-  const theme = await getJson("codesandbox-dark.json");
-  const langSource = await getJson("css.tmLanguage.json");
   return shiki.getHighlighter({
     theme,
-    langs: [{ id: "css", scopeName: "source.css", grammar: langSource }],
+    langs: [{ id: "css", scopeName: "source.css", grammar: css }],
   });
-};
-
-const getJson = async (path: string) => {
-  if (cache[path]) return cache[path];
-
-  const resp = await fetch(`https://css-weights.vercel.app/${path}`);
-  const body = resp.json();
-
-  cache[path] = body;
-  return body;
 };
 
 export const parseDocs = async () => {
-  readDir(__dirname);
   const resp = await fetch("https://css-weights.vercel.app/help.md");
   const text = await resp.text();
   return marked.parse(text);
